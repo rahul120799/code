3c784b5fd166e8ac332470fac747d8226b6515be
A. Samson <47207320+Samson26@users.noreply.github.com>
2019-02-01 00:12:04 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,10 @@
+c=int(input("Enter the number1:"))
+e=int(input("Enter the number2:"))
+f=int(input("Enter the number3:"))
+if(c>e):
+  if(c>f):
+    print(c)
+elif(e>f):
+  print(e)
+else:
+  print(f)

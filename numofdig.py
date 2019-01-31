ade6a05be35b09906e17564de6166276a0ed8b78
A. Samson <47207320+Samson26@users.noreply.github.com>
2019-02-01 02:42:28 +0530

    Update numofdig.py

--- numofdig.py
+++ numofdig.py
@@ -1,11 +1,6 @@
-user=int(raw_input())
-if user<=9 and user>=0:
-	print "1"
-elif user<=99 and user>=10:
-	print "2"
-elif user<=999 and user>=100:
-	print "3"
-elif user<=1000 and user>=9999:
-	print "4"
-else:
-	print "5"
+digits = 0
+number = int(input())
+while (number > 0):
+  number = number//10
+  digits = digits + 1
+print digits

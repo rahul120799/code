61bb02a135bb5a7f8a2fea624fdc589c29adcd82
A. Samson <47207320+Samson26@users.noreply.github.com>
2019-02-01 19:50:21 +0530

    Update same.py

--- same.py
+++ same.py
@@ -9,9 +9,9 @@ if e>=f:
 	for x in range(0,e):
 		for y in range(0,f):
 			if c[x]==d[y]:
-				print c[y],
+				print c[y]
 else:
 	for x in range(0,f):
 		for y in range(0,e):
 			if d[x]==c[y]:
-				print d[x],
+				print d[x]
